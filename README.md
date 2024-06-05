a77e1d53faf48b6f788b3210e1c4b461dea343e8
mimonelu <mimonelu@gmail.com>
2024-06-05 19:31:14 +0900

    Refactor

--- README.md
+++ README.md
@@ -1,42 +1,30 @@
-# Klearlist
+# ⭐ Klearlist
+Klearlist is a ATProtocol's PDS list. Note, this list is a partial, not an all.
+JSON file is [here](./list.json) .
+Term: 2024/6/4 19:27:31 - 2024/6/5 19:27:44
 
-Term: 5/29/2024, 8:23:30 AM - 6/5/2024, 8:24:06 AM
-
-* https://agaric.us-west.host.bsky.network : 3023
-* https://amanita.us-east.host.bsky.network : 110
-* https://angels.fan : 1
-* https://aspen.moe : 1
-* https://atproto.brid.gy : 615
-* https://b.hill.fo : 1
-* https://blewit.us-west.host.bsky.network : 2978
-* https://boletus.us-west.host.bsky.network : 2984
-* https://bs.lehtiin.eu : 3
-* https://bsky-pds.dsrs.it : 5
-* https://bsky.pavli.cc : 1
-* https://bsky.tochu-cha.mydns.jp : 3
-* https://bsky.vylion.com : 2
-* https://chaga.us-west.host.bsky.network : 3115
-* https://conocybe.us-west.host.bsky.network : 3100
-* https://enoki.us-east.host.bsky.network : 134
-* https://hydnum.us-west.host.bsky.network : 3023
-* https://inkcap.us-east.host.bsky.network : 114
-* https://kty.lol : 2
-* https://lepista.us-west.host.bsky.network : 2997
-* https://lionsmane.us-east.host.bsky.network : 108
-* https://maitake.us-west.host.bsky.network : 3113
-* https://morel.us-east.host.bsky.network : 134
-* https://nmatsui.work : 8
-* https://oyster.us-east.host.bsky.network : 124
-* https://pds.a2.rip : 1
+* https://agaric.us-west.host.bsky.network : 417
+* https://amanita.us-east.host.bsky.network : 16
+* https://atproto.brid.gy : 74
+* https://blewit.us-west.host.bsky.network : 430
+* https://boletus.us-west.host.bsky.network : 425
+* https://chaga.us-west.host.bsky.network : 455
+* https://conocybe.us-west.host.bsky.network : 437
+* https://enoki.us-east.host.bsky.network : 10
+* https://hydnum.us-west.host.bsky.network : 425
+* https://inkcap.us-east.host.bsky.network : 19
+* https://lepista.us-west.host.bsky.network : 473
+* https://lionsmane.us-east.host.bsky.network : 15
+* https://maitake.us-west.host.bsky.network : 460
+* https://morel.us-east.host.bsky.network : 20
+* https://oyster.us-east.host.bsky.network : 10
 * https://pds.mattobs.com : 1
-* https://pds.shreyanjain.net : 1
-* https://pds.tom-sherman.com : 2
-* https://pds.wyvern.rip : 1
-* https://porcini.us-east.host.bsky.network : 106
-* https://puffball.us-east.host.bsky.network : 108
-* https://russula.us-west.host.bsky.network : 2453
-* https://shiitake.us-east.host.bsky.network : 132
-* https://shimeji.us-east.host.bsky.network : 135
-* https://verpa.us-west.host.bsky.network : 2984
-* https://x.kty.lol : 2
-* https://yumeka.cc : 2
+* https://pds.tom-sherman.com : 1
+* https://porcini.us-east.host.bsky.network : 14
+* https://puffball.us-east.host.bsky.network : 14
+* https://russula.us-west.host.bsky.network : 17
+* https://shiitake.us-east.host.bsky.network : 21
+* https://shimeji.us-east.host.bsky.network : 9
+* https://verpa.us-west.host.bsky.network : 446
+
+Klearlist © 2024 [mimonelu](https://bsky.app/profile/mimonelu.net)
--- main.mjs
+++ main.mjs
@@ -4,18 +4,72 @@ const DAYS = 7
 const MAX_ITERATION = 70
 
 async function main () {
-  const list = await makeList()
-  createJson(list)
-  createReadMe(list)
+  const entireData = await makeEntireData()
+  createJson(entireData)
+  createReadMe(entireData)
 }
 
-async function makeList() {
+async function makeEntireData() {
   const startedAt = new Date()
   startedAt.setDate(startedAt.getDate() - DAYS)
-  const plcs = await fetchPlcs(startedAt, MAX_ITERATION)
+  const entireLogs = await fetchEntireLogs(startedAt, MAX_ITERATION)
+  const endpoints = makeEndpoints(entireLogs)
+  return {
+    startedAt: startedAt.toISOString(),
+    endpoints,
+  }
+}
 
+async function fetchEntireLogs (startedAt, maxIteration) {
+  const entireLogs = []
+  for (let i = 0; i < maxIteration; i ++) {
+    const logs = await fetchLogs(startedAt.toISOString(), 1000)
+    // console.log(startedAt, i, logs.length)
+    if (logs == null ||
+        logs.length <= 1
+    ) {
+      return entireLogs
+    }
+    const createdAt = logs.at(- 1)?.createdAt
+    if (createdAt == null) {
+      console.error("createdAt is null/undefined.", logs.at(- 1))
+      return entireLogs
+    }
+    startedAt = new Date(createdAt)
+    entireLogs.push(...logs)
+
+    // 💕 Drive safely...
+    await wait(1000)
+  }
+  return entireLogs
+}
+
+async function fetchLogs (after, count = 1000) {
+  const response = await fetch(
+    // SEE: https://web.plc.directory/spec/v0.1/did-plc
+    `https://plc.directory/export?after=${after}&count=${count}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => response)
+    .catch((error) => error)
+  if (response == null ||
+      response instanceof Error
+  ) {
+    console.error("fetchLogs is failed.", after)
+    return
+  }
+  return (await response.text())
+    ?.split("\n")
+    ?.map((text) => JSON.parse(text))
+}
+
+function makeEndpoints (entireLogs) {
   const endpointMap = new Map()
-  plcs.forEach((doc) => {
+  entireLogs.forEach((doc) => {
     const pds = doc.operation?.services?.atproto_pds
     if (pds?.type !== "AtprotoPersonalDataServer" ||
       !(pds?.endpoint)
@@ -31,7 +85,6 @@ async function makeList() {
       })
     }
   })
-
   const endpoints = Object.keys(Object.fromEntries(endpointMap))
     .map((key) => {
       const endpoint = endpointMap.get(key)
@@ -40,64 +93,39 @@ async function makeList() {
         count: endpoint.count,
       }
     })
-  endpoints.sort((a, b) => a.url < b.url ? - 1 : a.url > b.url ? 1 : 0)
 
-  return {
-    startedAt: startedAt.toISOString(),
-    endpoints,
-  }
+  // Sort by URL
+  endpoints.sort((a, b) => {
+    return a.url < b.url
+      ? - 1
+      : a.url > b.url
+        ? 1
+        : 0
+  })
+
+  return endpoints
 }
 
-async function fetchPlcs (startedAt, maxIteration) {
-  const plcs = []
-  for (let i = 0; i < maxIteration; i ++) {
-    const plc = await fetchPlc(startedAt.toISOString(), 1000)
-    // console.log(startedAt, i, plc.length)
-    if (plc == null || plc.length <= 1) {
-      return plcs
-    }
-    const createdAt = plc.at(- 1)?.createdAt
-    if (createdAt == null) {
-      console.error("createdAt is nullified.", plc.at(- 1))
-      return plcs
-    }
-    startedAt = new Date(createdAt)
-    plcs.push(...plc)
-    await wait(1000)
-  }
-  return plcs
+function createJson (entireData) {
+  fs.writeFileSync("./list.json", JSON.stringify(entireData), "utf8")
 }
 
-async function fetchPlc (after, count = 1000) {
-  const response = await fetch(
-    `https://plc.directory/export?after=${after}&count=${count}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  )
-    .then((response) => response)
-    .catch((error) => error)
-  if (response instanceof Error || response == null) {
-    console.error("fetchPlc is failed.", after)
-    return
-  }
-  return (await response.text())?.split("\n")?.map((text) => JSON.parse(text))
-}
+function createReadMe (entireData) {
+  const startedAt = new Date(entireData.startedAt).toLocaleString()
+  const endedAt = (new Date()).toLocaleString()
+  const list = entireData.endpoints.map((endpoint) => {
+    return `* ${endpoint.url} : ${endpoint.count}`
+  }).join("\n")
+  const readMe = `# ⭐ Klearlist
+Klearlist is a ATProtocol's PDS list. Note, this list is a partial, not an all.
+JSON file is [here](./list.json) .
+Term: ${startedAt} - ${endedAt}
 
-function createJson (list) {
-  fs.writeFileSync("./list.json", JSON.stringify(list), "utf8")
-}
+${list}
 
-function createReadMe (list) {
-  const readMe = ["# ⭐ Klearlist\n"]
-  readMe.push("Klearlist is a ATProtocol's PDS list. Note, this list is a partial, not an all.\n")
-  readMe.push("JSON file is [here](./list.json) .\n")
-  readMe.push(`Term: ${new Date(list.startedAt).toLocaleString()} - ${(new Date()).toLocaleString()}\n`)
-  readMe.push(...list.endpoints.map((endpoint) => `* ${endpoint.url} : ${endpoint.count}`))
-  readMe.push("\nKlearlist © 2024 mimonelu")
-  fs.writeFileSync("./README.md", readMe.join("\n"), "utf8")
+Klearlist © 2024 [mimonelu](https://bsky.app/profile/mimonelu.net)
+`
+  fs.writeFileSync("./README.md", readMe, "utf8")
 }
 
 async function wait (interval) {
